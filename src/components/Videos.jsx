47d0586a32d5de12b8d4325cca720d2926226ed0
amir-hp80 <146808047+amir-hp80@users.noreply.github.com>
2024-04-27 16:18:15 +0330

    initial commit

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,13 +1,14 @@
 import { Box, Stack } from '@mui/material'
 import React from 'react'
-
+import {VideoCard , ChannelCard} from './'
 const Videos = ({videos}) => {
   // console.log(videos)
   return (
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
       {videos?.map((e,index) =>(
         <Box key={index}>
-          {e.id.videoId && <VideoCard/>}
+          {e.id.videoId && <VideoCard video={e}/>}
+          {e.id.channelId && <ChannelCard channelDetail={e}/>}
         </Box>
       ))}
     </Stack>

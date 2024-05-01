69a55344e34f55535417c91631e65283ab1435ad
amir-hp80 <146808047+amir-hp80@users.noreply.github.com>
2024-05-01 12:02:12 +0330

    Update SearchFeed.jsx
    getting data and styling component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import { React, useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import Videos from "./Videos";
+import fetchFromAPI from "../utils/fetchFromAPI";
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams()
+  useEffect(() => {
+    (async () => {
+      const res = await fetchFromAPI(
+        `search?part=snippet&q=${searchTerm}&maxResults=50`
+      );
+      setVideos(res.items);
+    })();
+  }, [searchTerm]);
 
-export default SearchFeed
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
+        Search Results for :{" "}
+        <span style={{ color: "#f31503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
